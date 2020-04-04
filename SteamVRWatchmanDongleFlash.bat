0492bceb7b8c826b70f4432b7d80df91f6d486db
NGenesis <NGenesis@users.noreply.github.com>
2020-04-04 11:41:43 +0100

    Added menu with option to revert watchman dongle back to controller dongle
    User will now be prompted whether they want to flash a Steam Controller Dongle to a SteamVR Watchman Dongle or vice versa, with a warning message explaining additional steps that should be taken before flashing.

--- SteamVRWatchmanDongleFlash.bat
+++ SteamVRWatchmanDongleFlash.bat
@@ -8,11 +8,7 @@ ECHO Steam installation found at "%steampath:/=\%"
 SET "steamvrpath=%steampath:/=\%\steamapps\common\SteamVR\tools\lighthouse\bin\win32\lighthouse_watchman_update.exe"
 IF NOT EXIST %steamvrpath% (GOTO :SteamVRPathNotFound)
 ECHO SteamVR Watchman Dongle Updater utility found at "%steamvrpath%"
-PUSHD "%steampath:/=\%\steamapps\common\SteamVR"
-ECHO Running SteamVR Watchman Dongle Updater utility...
-tools\lighthouse\bin\win32\lighthouse_watchman_update.exe -D tools\lighthouse\firmware\vr_controller\archive\htc_vrc_dongle_1461100729_2016_04_19.bin
-POPD
-ECHO SteamVR Watchman Dongle Updater utility has finished.
+GOTO :Menu
 PAUSE
 EXIT /B
 
@@ -23,5 +19,47 @@ EXIT /B
 
 :SteamPathNotFound
 ECHO Error: Failed to find Steam installation path.
+PAUSE
+EXIT /B
+
+:Menu
+ECHO.
+ECHO -----------------------------------------------------------------------------
+ECHO -                     STEAM / STEAMVR DONGLE FLASHER                        -
+ECHO -----------------------------------------------------------------------------
+ECHO.
+ECHO WARNING: Before running this, ensure you do the following first:
+ECHO  - Exit SteamVR and Steam.
+ECHO  - Power off all VR headsets and VR controllers.
+ECHO  - Disconnect any dongles you don't want to flash from the computer.
+ECHO  - Connect any dongles you want to flash to the computer.
+ECHO.
+ECHO 1. Convert [ Steam Controller Dongle ] to [ SteamVR Watchman Dongle ]
+ECHO 2. Revert [ SteamVR Watchman Dongle ] back into [ Steam Controller Dongle ]
+ECHO 3. Exit
+ECHO.
+:RequestMenuOption
+SET /P option="Type an option from [ 1, 2, 3 ] then press ENTER: "
+IF NOT DEFINED option GOTO :RequestMenuOption
+IF %option%==1 GOTO :ConvertDongle
+IF %option%==2 GOTO :RevertDongle
+IF %option%==3 EXIT /B
+GOTO :RequestMenuOption
+
+:ConvertDongle
+PUSHD "%steampath:/=\%\steamapps\common\SteamVR\tools\lighthouse"
+ECHO Converting all connected Steam Controller Dongles to SteamVR Watchman Dongles...
+bin\win32\lighthouse_watchman_update.exe -D firmware\vr_controller\archive\htc_vrc_dongle_1461100729_2016_04_19.bin
+POPD
+ECHO Dongle flasher utility has finished.
+PAUSE
+EXIT /B
+
+:RevertDongle
+PUSHD "%steampath:/=\%\steamapps\common\SteamVR\tools\lighthouse"
+ECHO Reverting all connected SteamVR Watchman Dongles back into Steam Controller Dongles...
+bin\win32\lighthouse_watchman_update.exe -e firmware\vr_controller\archive\htc_vrc_dongle_1461100729_2016_04_19.bin
+POPD
+ECHO Dongle flasher utility has finished.
 PAUSE
 EXIT /B
